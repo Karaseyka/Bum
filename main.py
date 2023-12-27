f530082900e57713aeaabc5e011a5864a1a571f3
Karaseyka <vladchuprina12@mail.ru>
2023-12-27 12:20:29 +0300

    add bum and boxes

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def start_screen():
     font = pygame.font.Font(None, 30)
     text_coord = 50
     for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('black'))
+        string_rendered = font.render(line, 1, pygame.Color('white'))
         intro_rect = string_rendered.get_rect()
         text_coord += 10
         intro_rect.top = text_coord
@@ -56,4 +56,100 @@ def start_screen():
                 return  # начинаем игру
         pygame.display.flip()
         clock.tick(FPS)
+
+
 start_screen()
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+print(load_level("1.txt"))
+tile_images = {
+    'wall': load_image('box.png'),
+    'empty': load_image('grass.png')
+}
+player_image = load_image('mar.png')
+
+tile_width = tile_height = 50
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.rotate = False
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+# основной персонаж
+player = None
+
+# группы спрайтов
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Player(x, y)
+    # вернем игрока, а также размер поля в клетках
+    return new_player, x, y
+
+
+player, level_x, level_y = generate_level(load_level('1.txt'))
+lavel = load_level("1.txt")
+running = True
+pygame.mouse.set_visible(False)
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT and lavel[player.rect.y // 50][player.rect.x // 50 - 1] != "#":
+                player.image = pygame.transform.flip(player.image, not player.rotate, False)
+                player.rotate = True
+                player.rect.x -= 50
+            elif event.key == pygame.K_RIGHT and lavel[player.rect.y // 50][player.rect.x // 50 + 1] != "#":
+                player.image = pygame.transform.flip(player.image, player.rotate, False)
+                player.rotate = False
+                player.rect.x += 50
+            elif event.key == pygame.K_UP and lavel[player.rect.y // 50 - 1][player.rect.x // 50] != "#":
+                player.rect.y -= 50
+            elif event.key == pygame.K_DOWN and lavel[player.rect.y // 50 + 1][player.rect.x // 50] != "#":
+                player.rect.y += 50
+    screen.fill(pygame.Color("white"))
+    all_sprites.draw(screen)
+    tiles_group.draw(screen)
+    player_group.draw(screen)
+    all_sprites.update()
+    pygame.display.flip()
+    clock.tick(10)
